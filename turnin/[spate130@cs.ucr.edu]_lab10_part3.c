4c5d70b07b25e7827112378fe05bf8efebcf54b8
Smit417263 <56094552+Smit417263@users.noreply.github.com>
2020-11-22 23:01:06 -0800

    Update [spate130@cs.ucr.edu]_lab10_part3.c

--- turnin/[spate130@cs.ucr.edu]_lab10_part3.c
+++ turnin/[spate130@cs.ucr.edu]_lab10_part3.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo Link:
+ *	Demo Link: https://youtu.be/BNM73diQr7o
  */
 
 #include <avr/io.h>
